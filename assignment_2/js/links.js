f69f3af229bf0b1ace9c3da96ca22ead60af7f68
Joëlle Bosman <jl.bosman@live.nl>
2019-05-10 15:44:18 +0200

    Part of assignment 2

--- assignment_2/js/links.js
+++ assignment_2/js/links.js
@@ -1,10 +1,12 @@
-var links ="<ul><li><a href='https://www.google.com' target='_blank'>Google</a></li><li><a href='https://www.facebook.com' target='_blank'>Facebook</a></li><li><a href='https://twitter.com' target='_blank'>Twitter</a></li></ul>";
+var links = "<ul id='list'><li><a href='https://www.google.com' target='_blank'>Google</a></li><li><a href='https://www.facebook.com' target='_blank'>Facebook</a></li><li><a href='https://twitter.com' target='_blank'>Twitter</a></li></ul>";
+var inputfield = "<input type='text'/><input type='text'/><input type='button' value='Submit'/>";
 
 $(function() {
     $("#links").append(links);
-    var input = $('<input type="button" value="Fade in and out" />');
+    var input = $('<input type="button" value="Fade in and out" id="button"/>');
     input.appendTo($("#links"));
-    input.click(function() {
-        $("links").toggle();
+    $("#button").click(function() {
+        $("#list").toggle();
     });
+    inputfield.appendTo($("#links"));
 });
